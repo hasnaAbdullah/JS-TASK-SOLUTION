72bdb92069f5f334945773cf27b955c0450bc2f4
greenFruits <mdabdullah6548ctg@gmail.com>
2025-02-09 19:09:21 +0600

    solved this task

--- break/3-square break.js
+++ break/3-square break.js
@@ -1,3 +1,11 @@
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+let i = 2;
+while (i <= 100) {
+  if (Number.isInteger(Math.sqrt(i))) {
+    console.log(i);
+    break;
+  }
+  i++;
+}
